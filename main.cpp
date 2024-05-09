82048d316cec61915fc14154b5a23e5c5e3ac218
Anastasiia Dankevych <adankevych@kse.org.ua>
2024-05-09 23:09:48 +0300

    create helper-list

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 
 int main()
 {
+    printf("Hello, World!\n");
+
+    printf("1 - Append text symbols to the end \n"
+           "2 - Start the new line \n"
+           "3 - Use files to saving the information \n"
+           "4 - Use files to loading the information \n"
+           "5 - Print the current text to console \n"
+           "6 - Insert the text by line and symbol index \n"
+           "7 - Search \n");
 
     return 0;
 }
